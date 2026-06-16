7b4da0f22db37a223c737361423cff248094ec67
kingnation0920 <57610017+kingnation0920@users.noreply.github.com>
2026-06-17 08:33:56 +0900

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://naver-blog-index-lapurezadelaire.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'hourly',
+      priority: 1,
+    },
+  ];
+}
